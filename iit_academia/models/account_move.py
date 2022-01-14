ec9668a1cff00812e5a2a937bc0c8a7e91421867
Alexander Paiz <impulsaitgt@gmail.com>
2022-01-14 15:21:08 -0600

    Correccion de problema de mes, cambiar a grabarlo en base de datos

--- iit_academia/models/account_move.py
+++ iit_academia/models/account_move.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    estudiante_id = fields.Many2one(comodel_name='aca.estudiante', required=True)
+    estudiante_id = fields.Many2one(comodel_name='aca.estudiante')
