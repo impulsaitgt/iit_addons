eb96f88f348b7ca220ceaeb181652d022332630e
Alexander Paiz <impulsaitgt@gmail.com>
2022-02-24 10:45:57 -0600

    Finalizacion manejo Inmuebles

--- iit_lotificacion/models/inmueble.py
+++ iit_lotificacion/models/inmueble.py
@@ -7,9 +7,22 @@ class Inmueble(models.Model):
     direccion = fields.Char(string="Direccion")
     numero_de_escritura = fields.Char(string="Numero de escritura")
     precio_a_publico = fields.Float(string="Precio a publico",required=True, default=0)
-    precio_minimo = fields.Float(string="Precio a minimo",required=True, default=0)
+    precio_iva = fields.Float(string="Precio con IVA",required=True, default=0, readonly=True)
+    precio_minimo = fields.Float(string="Precio Minimo (Sin IVA)",required=True, default=0)
     reserva = fields.Float(string="Reserva",required=True, default=0)
+    frente = fields.Float(string="Frente (metros)", default=0)
+    lateral_izquierdo = fields.Float(string="Lateral Izquierdo (metros)", default=0)
+    fondo = fields.Float(string="Fondo (metros)", default=0)
+    lateral_derecho = fields.Float(string="Lateral (metros)", default=0)
+    area = fields.Float(string="Area (metros²)", default=0)
     foto = fields.Binary(string="Foto")
+
+    @api.onchange('precio_a_publico')
+    def onchange_precio_a_publico(self):
+        iva_id = self.env['account.tax'].search([("type_tax_use", "=", "sale")])
+        self.precio_iva = round(round(iva_id.amount / 100 * self.precio_a_publico, 2) + self.precio_a_publico, 2)
+
+
 
 
 
